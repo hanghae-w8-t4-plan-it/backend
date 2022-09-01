4373f808e731bd60460583c8949ab0708e0d008f
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-09-01 19:44:59 +0900

    fix: 자바 실행중인 경우 종료 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,16 @@ REPOSITORY=/home/ubuntu/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
