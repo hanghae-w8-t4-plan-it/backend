2cb562ddd4830bdcc2ccd09c6849886000c1c59a
ParkJong-ic <whddlrs@naver.com>
2022-09-01 15:14:35 +0900

    fix: 카테고리  변수명 수정(배포를 위해 follow부분 일부 커밋)

---
+++ src/main/java/com/team4/planit/follow/FollowRepository.java
@@ -0,0 +1,6 @@
+package com.team4.planit.follow;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+}
---
+++ src/main/java/com/team4/planit/follow/FollowService.java
@@ -0,0 +1,9 @@
+package com.team4.planit.follow;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+}
