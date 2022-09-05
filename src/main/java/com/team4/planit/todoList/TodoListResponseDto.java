26b6015ca8ec58669cc7e01434ae84bcbae56b65
god1hyuk <wonhyuk6340@gmail.com>
2022-09-06 02:34:00 +0900

    feat: 남은 Todo 개수 조회

---
+++ src/main/java/com/team4/planit/todoList/TodoListResponseDto.java
@@ -0,0 +1,17 @@
+package com.team4.planit.todoList;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TodoListResponseDto {
+    private String dueDate;
+    private Integer unAchievedTodoCnt;
+
+    public TodoListResponseDto(String dueDate, Integer unAchievedTodoCnt) {
+        this.dueDate = dueDate;
+        this.unAchievedTodoCnt = unAchievedTodoCnt;
+    }
+}
--- src/main/java/com/team4/planit/todoList/TodoListService.java
+++ src/main/java/com/team4/planit/todoList/TodoListService.java
@@ -4,18 +4,24 @@ import com.team4.planit.category.CategoryService;
 import com.team4.planit.global.shared.Check;
 import com.team4.planit.member.Member;
 import lombok.RequiredArgsConstructor;
+import com.team4.planit.global.shared.Message;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
-
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TodoListService {
     private final Check check;
+    private final TodoListRepositorySupport todoListRepositorySupport;
     private final TodoListRepository todoListRepository;
     private final CategoryService categoryService;
+    private final TodoRepository todoRepository;
 
     public ResponseEntity<?> getPlanet(TodoListRequestDto requestDto, HttpServletRequest request) {
         Member member = check.validateMember(request);
@@ -24,4 +30,19 @@ public class TodoListService {
         todoList.update(requestDto.getPlanet());
         return categoryService.getAllCategories(requestDto.getDueDate(), request);
     }
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<?> getTodoListByYearAndMonth(String year, String month, HttpServletRequest request) {
+        check.validateMember(request);
+        List<TodoList> todoLists = todoListRepositorySupport.findAllByYearAndMonth(year, month);
+        List<TodoListResponseDto> todoListResponseDtoList = new ArrayList<>();
+        for (TodoList todoList : todoLists) {
+            todoListResponseDtoList.add(
+                    new TodoListResponseDto(todoList.getDueDate(),
+                            todoRepository.countAllByTodoListAndIsAchieved(todoList, false))
+            );
+        }
+        return new ResponseEntity<>(Message.success(todoListResponseDtoList), HttpStatus.OK);
+    }
+
 }
