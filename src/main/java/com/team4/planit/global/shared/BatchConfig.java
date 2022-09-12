ea63be887b87b40252333f0d603baf909e71b7a5
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-09-12 22:35:56 +0900

    feat:배치 시작

---
+++ src/main/java/com/team4/planit/global/shared/BatchConfig.java
@@ -0,0 +1,85 @@
+package com.team4.planit.global.shared;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.Store;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class BatchConfig {
+
+    public static final String JOB_NAME = "todoBackupBatch";
+    private static final String STEP_NAME = JOB_NAME+"Step";
+
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Value("${chunkSize:1000}")
+    private int chunkSize; // Parameter로 chunkSize를 던지면 해당 값으로, 없으면 1000을 기본으로
+
+    private static String ADDRESS_PARAM = null;
+
+    // Job 생성
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step step() {
+        return stepBuilderFactory.get(STEP_NAME)
+                .<Store, StoreHistory>chunk(chunkSize)
+                .reader(reader(ADDRESS_PARAM))
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JpaPagingItemReader<Store> reader (
+            @Value("#{jobParameters[address]}") String address) {
+
+        Map<String, Object> parameters = new LinkedHashMap<>();
+        parameters.put("address", address+"%");
+
+        JpaPagingItemReader<Store> reader = new JpaPagingItemReader<>();
+        reader.setEntityManagerFactory(entityManagerFactory);
+        reader.setQueryString("select s From Store s where s.address like :address");
+        reader.setParameterValues(parameters);
+        reader.setPageSize(chunkSize);
+
+        return reader;
+    }
+
+    public ItemProcessor<Store, StoreHistory> processor() {
+        return item -> new StoreHistory(item, item.getProducts(), item.getEmployees());
+    }
+
+    public JpaItemWriter<StoreHistory> writer() {
+        JpaItemWriter<StoreHistory> writer = new JpaItemWriter<>();
+        writer.setEntityManagerFactory(entityManagerFactory);
+        return writer;
+    }
+}
