5e45dca989e647b20824cfdff8a37d242390a980
ParkJong-ic <whddlrs@naver.com>
2022-09-12 22:26:53 +0900

    fix: 타이머 시작시간 추가, 설정시간 및 남은시간 분단위로 수정

--- src/main/java/com/team4/planit/timer/TimerService.java
+++ src/main/java/com/team4/planit/timer/TimerService.java
@@ -11,18 +11,30 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
 public class TimerService {
     private final Check check;
+//    private final ConcentrateRateService concentrateRateService;
     private final TimerRepository timerRepository;
 
     @Transactional
     public ResponseEntity<?> createTimer(TimerRequestDto timerRequestDto, HttpServletRequest request) {
         Member member = check.validateMember(request);
-        Timer timer = new Timer(timerRequestDto.getSetTime(), timerRequestDto.getRemainTime(), member);
+        LocalDateTime startTime = LocalDateTime.now().minusMinutes(timerRequestDto.getSetTime() - timerRequestDto.getRemainTime());
+        Timer timer = Timer.builder()
+                .member(member)
+                .setTime(timerRequestDto.getSetTime())
+                .remainTime(timerRequestDto.getRemainTime())
+                .date(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(startTime))
+                .build();
         timerRepository.save(timer);
+//        concentrateRateService.createConcentrateRate(timer);
         return new ResponseEntity<>(Message.success(null), HttpStatus.OK);
     }
 }
+
+// 분데이터로만 받고
--- src/main/java/com/team4/planit/timer/dto/TimerRequestDto.java
+++ src/main/java/com/team4/planit/timer/dto/TimerRequestDto.java
@@ -1,15 +1,17 @@
 package com.team4.planit.timer.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class TimerRequestDto {
-    private String setTime;
-    private String remainTime;
+    private Integer setTime;
+    private Integer remainTime;
 
-    public TimerRequestDto(String setTime, String remainTime) {
+    @Builder
+    public TimerRequestDto(Integer setTime, Integer remainTime) {
         this.setTime = setTime;
         this.remainTime = remainTime;
     }
