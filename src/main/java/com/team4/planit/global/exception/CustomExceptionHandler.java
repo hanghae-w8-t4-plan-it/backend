9c8370d2a8c42a911b45e9c83b401e2130c2b28c
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-09-14 20:09:52 +0900

    feat: 잘못된 값 입력 예외처리

--- src/main/java/com/team4/planit/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/team4/planit/global/exception/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.team4.planit.global.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.team4.planit.global.shared.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,5 +22,9 @@ public class CustomExceptionHandler {
                 .get(0)
                 .getDefaultMessage();
         return new ResponseEntity<>(Message.fail("BAD_REQUEST", errorMessage), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(InvalidFormatException.class)
+    protected ResponseEntity<?> handlingInvalidFormatException(InvalidFormatException e) {
+        return new ResponseEntity<>(Message.fail("INVALID_FORMAT", "잘못된 값을 입력하셨습니다."), HttpStatus.BAD_REQUEST);
     }
 }
