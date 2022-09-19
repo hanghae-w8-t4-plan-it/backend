ce187c42668ef609f53da19f5c737da59aef848a
god1hyuk <wonhyuk6340@gmail.com>
2022-09-19 13:38:37 +0900

    refactor: TodoController 리턴값 변경

--- src/main/java/com/team4/planit/todo/TodoController.java
+++ src/main/java/com/team4/planit/todo/TodoController.java
@@ -1,12 +1,16 @@
 package com.team4.planit.todo;
 
+import com.team4.planit.global.shared.Message;
 import com.team4.planit.todo.dto.TodoRequestDto;
+import com.team4.planit.todo.dto.TodoResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.ParseException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,22 +21,26 @@ public class TodoController {
     @PostMapping("/categories/{categoryId}/todos")
     public ResponseEntity<?> createTodo(@PathVariable Long categoryId, @RequestBody TodoRequestDto requestDto,
                                         HttpServletRequest request) {
-        return todoService.createTodo(categoryId, requestDto, request);
+        TodoResponseDto todoResponseDto = todoService.createTodo(categoryId, requestDto, request);
+        return new ResponseEntity<>(Message.success(todoResponseDto), HttpStatus.OK);
     }
 
     @GetMapping("/categories/todos")
     public ResponseEntity<?> getAllTodos(HttpServletRequest request) {
-        return todoService.getAllTodos(request);
+        List<Todo> todos = todoService.getAllTodos(request);
+        return new ResponseEntity<>(Message.success(todos), HttpStatus.OK);
     }
 
     @PatchMapping("/categories/todos/{todoId}")
     public ResponseEntity<?> updateTodo(@PathVariable Long todoId, @RequestBody TodoRequestDto requestDto,
                                         HttpServletRequest request) throws ParseException {
-        return todoService.updateTodo(todoId, requestDto, request);
+        TodoResponseDto todoResponseDto = todoService.updateTodo(todoId, requestDto, request);
+        return new ResponseEntity<>(Message.success(todoResponseDto), HttpStatus.OK);
     }
 
     @DeleteMapping("/categories/todos/{todoId}")
     public ResponseEntity<?> deleteTodo(@PathVariable Long todoId, HttpServletRequest request) {
-        return todoService.deleteTodo(todoId, request);
+        todoService.deleteTodo(todoId, request);
+        return new ResponseEntity<>(Message.success(null), HttpStatus.OK);
     }
 }
