bd29e637b27af92eced20d29d9d9289a8a023eb9
god1hyuk <wonhyuk6340@gmail.com>
2022-09-19 23:27:41 +0900

    feat: 행성꾸미기 기능 완료

--- src/main/java/com/team4/planit/todoList/dto/TodoListRequestDto.java
+++ src/main/java/com/team4/planit/todoList/dto/TodoListRequestDto.java
@@ -7,9 +7,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TodoListRequestDto {
     String dueDate;
-    String planet;
-    public TodoListRequestDto(String dueDate,String planet){
-        this.dueDate=dueDate;
-        this.planet=planet;
+    Byte planetType;
+    Short planetSize;
+    Byte planetColor;
+
+    public TodoListRequestDto(String dueDate, Byte planetType) {
+        this.dueDate = dueDate;
+        this.planetType = planetType;
+    }
+
+    public TodoListRequestDto(String dueDate, Short planetSize, Byte planetColor) {
+        this.dueDate = dueDate;
+        this.planetSize = planetSize;
+        this.planetColor = planetColor;
     }
 }
