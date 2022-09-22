130c0fa255a624991821e77036682e4c4f8f4f14
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-09-22 17:37:27 +0900

    fix: batch 비활성화

--- src/main/java/com/team4/planit/global/shared/Scheduler.java
+++ src/main/java/com/team4/planit/global/shared/Scheduler.java
@@ -6,9 +6,14 @@ import com.team4.planit.statistic.achievement.AchievementRepository;
 import com.team4.planit.statistic.concentration.ConcentrationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -21,8 +26,14 @@ public class Scheduler {
     private final AchievementRepository achievementRepository;
 
     @Scheduled(cron = "0 50 3 * * *")
-    public void updateStatistic() {
+    public void updateStatistic() throws ParseException {
         List<Member> memberList = memberRepository.findAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date temp = sdf.parse(String.valueOf(DateTime.now()));
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(temp);
+        cal.get(Calendar.WEEK_OF_YEAR);
+        String endDate = sdf.format(cal.getTime());
         for (Object member : memberList ) {
             //집중도 구하기 (%)
 
