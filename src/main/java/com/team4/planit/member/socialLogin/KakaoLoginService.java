3b6aad1f3f851bc6651a3795c2c2996359bd72dc
ParkJong-ic <whddlrs@naver.com>
2022-10-01 01:37:07 +0900

    fix: 카카오로그인주소 변경

--- src/main/java/com/team4/planit/member/socialLogin/KakaoLoginService.java
+++ src/main/java/com/team4/planit/member/socialLogin/KakaoLoginService.java
@@ -83,7 +83,7 @@ public class KakaoLoginService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", myKaKaoRestAplKey);
-        body.add("redirect_uri", "http://localhost:3000/kakao");
+        body.add("redirect_uri", "http://localhost:3000/login/kakao");
         body.add("code", code);
 
         // HTTP 요청 보내기
